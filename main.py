0cd72ea969af3c1520cf0da42b18fef18d33488a
needleworm <bhban@kakao.com>
2021-08-16 10:03:03 +0900

    .

--- main.py
+++ main.py
@@ -13,10 +13,17 @@ def find_and_click(filename):
     test = pw.find_on_screen(filename, confidence=0.7)
     if test:
         pw.click(test)
+        if filename == "rankMedal.JPG":
+            time.sleep(2)
+            pw.click(test)
+        elif filename == "leagueStart.JPG":
+            time.sleep(200)
+    else:
+        return False
 
 
 while True:
     for el in imgList:
         find_and_click(el)
-        time.sleep(10)
+        time.sleep(1)
 
